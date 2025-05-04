d3e591083c0d094d835f23cee4cf80a69835fbde
Pearse Hutson <pearse.2008@gmail.com>
2025-05-04 16:19:31 +0000

    Replacing nvim config with LazyVim starter

--- config/nvim/lua/core/options.lua
+++
@@ -1,8 +0,0 @@
--- lua/core/options.lua
-vim.opt.number = true         -- show line numbers
-vim.opt.relativenumber = true -- relative line numbers
-vim.opt.tabstop = 2           -- tab size = 2 spaces
-vim.opt.shiftwidth = 2        -- indentation = 2 spaces
-vim.opt.expandtab = true      -- convert tabs to spaces
-vim.opt.clipboard = "unnamedplus" -- use system clipboard
-vim.opt.scrolloff = 5         -- minimum lines to keep above/below cursor
